e9498302832cc489632a71b243cce78414d4f2f2
Fatih BARAÇKILIÇ <24433692+FatihBARACKILIC@users.noreply.github.com>
2026-09-15 17:57:34 +0300

    Initial Commit

---
+++ AudioManager/AudioManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioManagerApp.swift
+//  AudioManager
+//
+//  Created by fatih on 15.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
